5e1571a2e7615675f69ef539e2b59ca9f0ec5ba4
Keith S Thomas <kthomasnj@hotmail.com>
2022-09-25 13:06:25 -0400

    Added GET functionality for Categories.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,13 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({include: Product}).then(data => res.json(data));
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk(req.params.id, {include: Product}).then(data => res.json(data));
 });
 
 router.post('/', (req, res) => {
