343002bedc483840bc2c8a3ce56efad18ed90235
Keith S Thomas <kthomasnj@hotmail.com>
2022-09-25 21:12:53 -0400

    Added onDelete Cascade for deleting issue.

--- models/index.js
+++ models/index.js
@@ -6,24 +6,28 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignkey: "category_id"
+  foreignkey: "category_id",
+  onDelete: 'CASCADE'
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignkey: "category_id"
+  foreignkey: "category_id",
+  onDelete: 'CASCADE'
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: "product_tag",
-  foreignKey: "product_id"
+  foreignKey: "product_id",
+  onDelete: 'CASCADE'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: "product_tag",
-  foreignkey: "tag_id"
+  foreignkey: "tag_id",
+  onDelete: 'CASCADE'
 });
 
 module.exports = {
